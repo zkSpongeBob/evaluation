c61d127b9fd9aa071ab8641f7d8b7a00f06711ae
zkSpongeBob <zkSpongeBob@126.com>
2023-07-31 17:59:52 +0800

    项目搬迁至GitHub

--- BriefEvaluation/__version__.py
+++ BriefEvaluation/__version__.py
@@ -1,6 +1,6 @@
 __package__ = 'BriefEvaluation'
 __description__ = 'This is a package for doing a comprehensive evaluation'
-__url__ = 'https://gitee.com/zkspongebob/evaluation'
+__url__ = 'https://github.com/zkSpongeBob/evaluation'
 __version__ = "0.0.2"
 __version_info__ = (0, 0, 2, 0)
 __author__ = 'zkSpongeBob'
